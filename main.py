24e7c82cd0f6a79a9958c81b5413e15c38ac1b64
Anton Hummel <anton@hummel.li>
2019-02-28 01:16:19 +0100

    algo

--- main.py
+++ main.py
@@ -18,58 +18,35 @@ class Transfer:
     def __str__(self):
         return str(self._amount) + " | " + self._date
 
-# DEPRECATED
-# def calculateBill(billsList, transferList):
-#     if (billsList.__len__() != 0 and transferList.__len__() != 0):
-#         for i in transferList:
-#             oldSumOfBills = 0
-#             sumOfBills = 0
-#             bestFitBills = []
-#             bestFitBillsSum = 0
-#             for j in billsList:
-#                 sumOfBills += j._amount
-#                 if (sumOfBills == i._amount):
-#                     bestFitBills.append(j)
-#                     bestFitBillsSum = sumOfBills
-#                 elif (sumOfBills > i._amount):
-#                     bestFitBill = oldSumOfBills
-#                     break
-#                 elif (sumOfBills < i._amount and sumOfBills > bestFitBillsSum):
-#                     bestFitBills.append(j)
-#                     continue
-#                 oldSumOfBills = sumOfBills
-#             for bills in bestFitBills:
-#                 print(bills.__str__())
-#     else:
-#         print("Error: Leere Rechnungs- oder Überweisungsliste!")
-
 def calculateBill2(bills, transfers):
     if (bills.__len__() != 0 and transfers.__len__() != 0):
         for i in transfers:
-            oldSumOfBills = 0
             bestFitBills = ()
             bestFitBillsSum = 0
-            billfound = False
+            skip = False
             for j in range(1, bills.__len__()+1):
-                if (not billfound):
-                    #print("-------------------------------------------------------------")
-                    comb = combinations(bills, j)
-                    for k in list(comb):
-                        sumOfBills = 0
-                        #print("Tupel: ---------------------------------------------")
+                comb = combinations(bills, j)
+                for k in list(comb):
+                    oldSumOfBills = 0
+                    sumOfBills = 0
+                    if (not skip):
                         for l in k:
                             sumOfBills += float(l._amount)
                             if (sumOfBills == i._amount):
                                 bestFitBills = k
                                 bestFitBillsSum = sumOfBills
-                                billfound = True
-                            if (sumOfBills < i._amount and sumOfBills > bestFitBillsSum):
+                                skip = True # Skip, cause Bills found
+                            elif (sumOfBills < i._amount and sumOfBills > bestFitBillsSum):
                                 bestFitBillsSum = sumOfBills
                                 bestFitBills = k
-                    billfound = True
-            print("SumOfBills: " + str(sumOfBills))
+                            elif (sumOfBills > i._amount):
+                                sumOfBills = oldSumOfBills
+                            oldSumOfBills = sumOfBills
+
+            print("    TransferAmount: " + str(i._amount))
+            print("    BestFitAmount: " + str(bestFitBillsSum))
             for l in bestFitBills:
-                print(l.__str__())
+                print("    " + l.__str__())
     else:
         print("Error: Leere Rechnungs- oder Überweisungsliste!")
 
@@ -96,5 +73,5 @@ if __name__ == '__main__':
     for i in transferList:
         print(i.__str__())
 
-    print("Kalkulation: ")
+    print("Kalkulation: ----------------------------------------")
     calculateBill2(billsList, transferList)
